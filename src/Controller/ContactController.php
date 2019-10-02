0e7d8c8b6ebfed82456efa25b960393654f66ad0
sabinebaer <sabine.baer@massiveart.com>
2019-10-02 11:28:49 +0200

    Added swagger annotations and enabled webview

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Contact;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\EntityRepository;
+use Swagger\Annotations as SWG;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -31,6 +32,34 @@ class ContactController
 
     /**
      * @Route("/api/contacts/{id}", name="contacts_get", methods={"GET"}, requirements={"id"="\d+"})
+     *
+     * @SWG\Get(
+     *     summary="Returns a specific contact.",
+     *     produces={"application/json"},
+     *     @SWG\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         type="integer"
+     *     ),
+     *     @SWG\Response(
+     *         response=200,
+     *         description="Returns status 200 and the contact.",
+     *         @SWG\Schema(
+     *             type="object",
+     *             properties={
+     *                 @SWG\Property(property="id", type="integer"),
+     *                 @SWG\Property(property="name", type="string"),
+     *                 @SWG\Property(property="email", type="string"),
+     *                 @SWG\Property(property="country", type="string")
+     *             }
+     *         )
+     *     ),
+     *     @SWG\Response(
+     *         response=404,
+     *         description="Returns status 404 if there is no contact with the given id."
+     *     )
+     * )
      */
     public function get(int $id): JsonResponse
     {
@@ -44,6 +73,53 @@ class ContactController
 
     /**
      * @Route("/api/contacts", name="contacts_post", methods={"POST"})
+     *
+     * @SWG\Post(
+     *     summary="Adds a new contact.",
+     *     produces={"application/json"},
+     *     @SWG\Parameter(
+     *         name="body",
+     *         description="Post data.",
+     *         in="body",
+     *         required=true,
+     *         @SWG\Schema(
+     *             type="object",
+     *             required={"name", "country"},
+     *             @SWG\Property(
+     *                 property="name",
+     *                 type="string",
+     *                 minLength=1,
+     *                 example="Mia Muster"
+     *             ),
+     *             @SWG\Property(
+     *                 property="email",
+     *                 type="string",
+     *                 format="email",
+     *                 example="mia@muster.com"
+     *             ),
+     *             @SWG\Property(
+     *                 property="country",
+     *                 description="ISO-2 country code in capital letters.",
+     *                 type="string",
+     *                 pattern="^[A-Z]{2}$",
+     *                 example="AT"
+     *             )
+     *         )
+     *     ),
+     *     @SWG\Response(
+     *         response=200,
+     *         description="Returns status 200 and the new contact.",
+     *         @SWG\Schema(
+     *             type="object",
+     *             properties={
+     *                 @SWG\Property(property="id", type="integer"),
+     *                 @SWG\Property(property="name", type="string"),
+     *                 @SWG\Property(property="email", type="string"),
+     *                 @SWG\Property(property="country", type="string")
+     *             }
+     *         )
+     *     )
+     * )
      */
     public function postAction(Request $request): JsonResponse
     {
@@ -58,6 +134,58 @@ class ContactController
 
     /**
      * @Route("/api/contacts/{id}", name="contacts_put", methods={"PUT"}, requirements={"id"="\d+"})
+     *
+     * @SWG\Put(
+     *     summary="Updates an existing contact.",
+     *     produces={"application/json"},
+     *     @SWG\Parameter(name="id", in="path", required=true, type="integer"),
+     *     @SWG\Parameter(
+     *         name="body",
+     *         description="Post data.",
+     *         in="body",
+     *         required=true,
+     *         @SWG\Schema(
+     *             type="object",
+     *             required={"name", "country"},
+     *             @SWG\Property(
+     *                 property="name",
+     *                 type="string",
+     *                 minLength=1,
+     *                 example="Mia Muster"
+     *             ),
+     *             @SWG\Property(
+     *                 property="email",
+     *                 type="string",
+     *                 format="email",
+     *                 example="mia@muster.com"
+     *             ),
+     *             @SWG\Property(
+     *                 property="country",
+     *                 description="ISO-2 country code in capital letters.",
+     *                 type="string",
+     *                 pattern="^[A-Z]{2}$",
+     *                 example="AT"
+     *             )
+     *         )
+     *     ),
+     *     @SWG\Response(
+     *         response=200,
+     *         description="Returns status 200 and the modified contact.",
+     *         @SWG\Schema(
+     *             type="object",
+     *             properties={
+     *                 @SWG\Property(property="id", type="integer"),
+     *                 @SWG\Property(property="name", type="string"),
+     *                 @SWG\Property(property="email", type="string"),
+     *                 @SWG\Property(property="country", type="string")
+     *             }
+     *         )
+     *     ),
+     *     @SWG\Response(
+     *         response=404,
+     *         description="Returns status 404 if there is no contact with the given id."
+     *     )
+     * )
      */
     public function putAction(Request $request, int $id): JsonResponse
     {
@@ -74,6 +202,25 @@ class ContactController
 
     /**
      * @Route("/api/contacts/{id}", name="contacts_delete", methods={"DELETE"}, requirements={"id"="\d+"})
+     *
+     * @SWG\Delete(
+     *     summary="Deletes the given contact.",
+     *     produces={"application/json"},
+     *     @SWG\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         type="integer"
+     *     ),
+     *     @SWG\Response(
+     *         response=200,
+     *         description="Returns status 200 if the contact was deleted."
+     *     ),
+     *     @SWG\Response(
+     *         response=404,
+     *         description="Returns status 404 if there is no contact with the given id."
+     *     )
+     * )
      */
     public function deleteAction(int $id)
     {
